15769045f962dd1e8e375240f9b55f6d72d1c9b1
Justin Eldridge <eldridgejm@gmail.com>
2023-10-22 17:21:35 -0700

    Added paragraph renderer.

--- practicebank/renderers/html/problem.py
+++ practicebank/renderers/html/problem.py
@@ -9,71 +9,91 @@ _Node = typing.Union[types.InternalNode, types.LeafNode]
 
 _RENDERERS = {}
 
+
 def _renderer(name: _Node):
     def decorator(fn):
         _RENDERERS[name] = fn
         return fn
+
     return decorator
+
 
 @_renderer(types.Problem)
 def _(node: types.Problem):
     contents = "\n".join(_render_node(child) for child in node.children())
     return f'<div class="problem">{contents}</div>'
 
+
 @_renderer(types.NormalText)
 def _(node: types.NormalText):
     return node.text
 
+
 @_renderer(types.BoldText)
 def _(node: types.BoldText):
-    return f'<b>{node.text}</b>'
+    return f"<b>{node.text}</b>"
+
 
 @_renderer(types.ItalicText)
 def _(node: types.ItalicText):
-    return f'<i>{node.text}</i>'
+    return f"<i>{node.text}</i>"
+
 
 @_renderer(types.Code)
 def _(node: types.Code):
     return f'<pre class="code">{node.code}</pre>'
 
+
 @_renderer(types.InlineCode)
 def _(node: types.InlineCode):
     return f'<span class="code">{node.code}</span>'
+
 
 @_renderer(types.FillInTheBlank)
 def _(node: types.FillInTheBlank):
     return f'<div class="fill-in-the-blank"><input type="text" class="fill-in-the-blank" /></div>'
 
+
 @_renderer(types.InlineMath)
 def _(node: types.InlineMath):
     return f'<span class="math">${node.latex}$</span>'
+
 
 @_renderer(types.DisplayMath)
 def _(node: types.DisplayMath):
     return f'<div class="math">\\[{node.latex}\\]</div>'
 
+
 @_renderer(types.TrueFalse)
 def _(node: types.TrueFalse):
-    return dedent("""
+    return dedent(
+        """
         <div class="true-false">
             <input type="radio" name="true-false" value="true" /> True
             <input type="radio" name="true-false" value="false" /> False
         </div>
-    """)
+    """
+    )
+
 
 @_renderer(types.Solution)
 def _(node: types.Solution):
     contents = "\n".join(_render_node(child) for child in node.children())
-    return f'<details><summary>Solution</summary>{contents}</details>'
+    return f"<details><summary>Solution</summary>{contents}</details>"
+
 
 @_renderer(types.Subproblem)
 def _(node: types.Subproblem):
     contents = "\n".join(_render_node(child) for child in node.children())
     return f'<div class="subproblem">{contents}</div>'
 
+
 @_renderer(types.Image)
 def _(node: types.Image):
-    return f'<center><div class="image"><img src="{node.relative_path}" /></div></center>'
+    return (
+        f'<center><div class="image"><img src="{node.relative_path}" /></div></center>'
+    )
+
 
 @_renderer(types.MultipleChoices)
 def _(node: types.MultipleChoices):
@@ -87,10 +107,16 @@ def _(node: types.MultipleSelect):
     contents = "\n".join(_render_choice(child, "checkbox") for child in node.children())
     return f'<div class="multiple-select">{contents}</div>'
 
+
+@_renderer(types.Paragraph)
+def _(node: types.Paragraph):
+    contents = "".join(_render_node(child) for child in node.children())
+    return f"<p>{contents}</p>"
+
+
 def _render_choice(node: types.Choice, kind: str):
     contents = "\n".join(_render_node(child) for child in node.children())
     return f'<div class="choice"><input type="{kind}" />{contents}</div>'
-
 
 
 def _render_node(node: _Node):
@@ -98,6 +124,7 @@ def _render_node(node: _Node):
         return _RENDERERS[type(node)](node)
     else:
         raise NotImplementedError(f"no renderer for {type(node)}")
+
 
 def render(problem: types.Problem):
     return _render_node(problem)
