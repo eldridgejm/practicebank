feefa24ee28a3dbb42bf39575d6d043a0ce01b1c
Justin Eldridge <eldridgejm@gmail.com>
2023-11-12 19:18:53 -0800

    Added panprob to flake.

--- flake.nix
+++ flake.nix
@@ -3,9 +3,12 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
+  inputs.panprob.url = github:eldridgejm/panprob/main;
+
   outputs = {
     self,
     nixpkgs,
+    panprob
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
@@ -18,20 +21,7 @@
             src = ./.;
             propagatedBuildInputs = with python3Packages; [
               pyyaml
-
-              # texsoup
-              (
-                pkgs.python3Packages.buildPythonPackage rec {
-                  pname = "texsoup";
-                  version = "0.3.1";
-                  name = "${pname}-${version}";
-                  src = builtins.fetchurl {
-                    url = "https://files.pythonhosted.org/packages/84/58/1c503390ed1a81cdcbff811dbf7a54132994acef8dd2194d55cf657a9e97/TexSoup-0.3.1.tar.gz";
-                    sha256 = "02xpvmhy174z6svpghzq4gs2bsyh0jxc2i7mark8ls73mg82lsrz";
-                  };
-                  doCheck = false;
-                }
-              )
+              panprob.defaultPackage.${system}
             ];
             nativeBuildInputs = with python3Packages; [pytest sphinx sphinx_rtd_theme pip];
             doCheck = true;
